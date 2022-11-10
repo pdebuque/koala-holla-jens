2094ad6113cc81df167a01893140ebd096acfd79
Paolo Debuque <pdebuque@gmail.com>
2022-11-10 14:14:53 -0600

    completed POST request server side

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -8,24 +8,38 @@ const pool = require('../modules/pool')
 
 // GET
 
-router.get('/', (req,res) =>{
+router.get('/', (req, res) => {
     console.log('in router get', req)
     let queryText = 'SELECT * FROM "koalas_list";';  // is this right?
     pool.query(queryText)
-    .then((result) => {
-        console.log('its working!',result.rows)
-        res.send(result.rows);
-    })
-    .catch((error) => {
-        console.log('where are the koalas', error);
-        res.sendStatus(500);
-    })
-
+        .then((result) => {
+            console.log('its working!', result.rows)
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('where are the koalas', error);
+            res.sendStatus(500);
+        })
 })
 
 // POST
 
-
+router.post('/', (req, res) => {
+    console.log('in POST request at /koalas');
+    const newKoala = req.body;
+    const queryText = `INSERT INTO koalas_list
+                        (name,age,gender,ready_for_transfer,notes)
+                        VALUES ('$1', $2, '$3', $4, '$5')
+    `
+    pool.query(queryText, [newKoala.name, newKoala.age, newKoala.gender, newKoala.ready_for_transfer, newKoala.notes])
+        .then(() => {
+            console.log('POST successful');
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('POST went wrong ', err)
+        })
+})
 
 // PUT
 
