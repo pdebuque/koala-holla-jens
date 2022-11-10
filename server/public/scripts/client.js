ece4a61d35eb71c92d970900fb554e1275b7d7db
Paolo Debuque <pdebuque@gmail.com>
2022-11-10 13:04:11 -0600

    restructured client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,64 +1,43 @@
-console.log( 'js' );
+console.log('js');
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
+$(onReady)
 
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    let koalaToSend = {
-      name: $('#nameIn').val(),
-      age: $('#ageIn').val(),
-      gender: $('#genderIn').val(),
-      readyForTransfer: $('#readyForTransferIn').val(),
-      notes: $('#notesIn').val(),
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
-  
-  // Establish Click Listeners
-  setupClickListeners()
+function onReady() {
+  console.log('JQ');
+  setupClickListeners();
+
   // load existing koalas on page load
   getKoalas();
 
-}); // end doc ready
+}; // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    let koalaToSend = {
-      name: $('#nameIn').val(),
-      age: $('#ageIn').val(),
-      gender: $('#genderIn').val(),
-      readyForTransfer: $('#readyForTransferIn').val(),
-      notes: $('#notesIn').val(),
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+  $('#addButton').on('click', addKoala);
+
+
 }
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
+function getKoalas() {
+  console.log('in getKoalas');
   // ajax call to server to get koalas
   $.ajax({
     type: 'GET',
     url: `/koalas`
-  }).then(function(response){
-    
-  }).catch(function(error){
-
+  }).then(function (response) {
+    renderDisplay(array);
+  }).catch(function (error) {
+    console.log('could not get koalas ', error)
   })
 } // end getKoalas
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
+function addKoala() {
+  console.log('in addKoala', newKoala);
   // ajax call to server to post koalas
-  
+
+
+
 }
 
-function renderKoalas(){
+function renderDisplay(array) {
 
 }
