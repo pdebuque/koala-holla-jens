cee0ca8a7bc9bb9970466c92fb8c07bbeb7f8fd5
Tracey <tracey@Traceys-MacBook-Air.local>
2022-11-10 12:56:13 -0600

    client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,21 @@ console.log( 'js' );
 
 $( document ).ready( function(){
   console.log( 'JQ' );
+
+  $( '#addButton' ).on( 'click', function(){
+    console.log( 'in addButton on click' );
+    // get user input and put in an object
+    let koalaToSend = {
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      gender: $('#genderIn').val(),
+      readyForTransfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val(),
+    };
+    // call saveKoala with the new obejct
+    saveKoala( koalaToSend );
+  }); 
+  
   // Establish Click Listeners
   setupClickListeners()
   // load existing koalas on page load
@@ -13,14 +28,12 @@ function setupClickListeners() {
   $( '#addButton' ).on( 'click', function(){
     console.log( 'in addButton on click' );
     // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
     let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      gender: $('#genderIn').val(),
+      readyForTransfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val(),
     };
     // call saveKoala with the new obejct
     saveKoala( koalaToSend );
@@ -30,11 +43,22 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  $.ajax({
+    type: 'GET',
+    url: `/koalas`
+  }).then(function(response){
+    
+  }).catch(function(error){
+
+  })
 } // end getKoalas
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
- 
+  // ajax call to server to post koalas
+  
+}
+
+function renderKoalas(){
+
 }
