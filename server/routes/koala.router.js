adb9f75d18208ea29eff3586645bf0f9ee833802
Paul Norling <paulnorling87@gmail.com>
2022-11-10 14:03:20 -0600

    add back

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,13 +1,30 @@
+const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
 
+const pool = require('../modules/pool')
 
 // GET
 
+router.get('/', (req,res) =>{
+    console.log('in router get', req)
+    let queryText = 'SELECT * FROM "koalas_list";';  // is this right?
+    pool.query(queryText)
+    .then((result) => {
+        console.log('its working!',result.rows)
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log('where are the koalas', error);
+        res.sendStatus(500);
+    })
+
+})
 
 // POST
+
 
 
 // PUT
